5e0d078b8b4ed7ab6fd207f8bbddba1112dd3b1c
Alvish6654 <alvish.pipaliya@technomark.io>
2023-12-11 10:27:10 +0530

    Email change

--- src/components/Textfield/Email.tsx
+++ src/components/Textfield/Email.tsx
@@ -60,7 +60,7 @@ const Email: React.FC<EmailProps> = ({
   }, [hasError, errorMessage]);
 
   const validateEmail = (email: string) => {
-    const regex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return regex.test(email);
   };
 
